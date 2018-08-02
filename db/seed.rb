0e01c18828bddfbb992de7904ce4554eeddb13c0
Jason Li <jason.li0102@gmail.com>
2018-08-02 09:36:30 -0700

    Allow flexibility of using sqlite3 or pg

--- db/seed.rb
+++ db/seed.rb
@@ -2,7 +2,7 @@ require_relative '../models/address_book'
 require_relative '../models/entry'
 require 'bloc_record'
 
-BlocRecord.connect_to('db/address_bloc.sqlite')
+BlocRecord.connect_to("db/address_bloc.db", :sqlite)
 
 book = AddressBook.create(name: 'My Address Book')
 
